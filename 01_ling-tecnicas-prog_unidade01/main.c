18d5e0d7c743da18336c9ab077eb38449fec02ef
Bernardo <mrochabernardo@gmail.com>
2022-10-12 08:13:38 -0300

    básico do básico em C

--- 01_ling-tecnicas-prog_unidade01/main.c
+++ 01_ling-tecnicas-prog_unidade01/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void main()
 {
-    printf("Hello word");
+    printf("Hello word\n");
+
+    char letra = 'm';
+    int idade = 78;
+    const float PI = 3.141592;
+    bool tomei_cafe = false;
+    
+
+
 }
