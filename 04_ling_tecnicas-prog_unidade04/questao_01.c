0ba6a5938bb91101297808cd32a981842e8be676
Bernardo <mrochabernardo@gmail.com>
2022-10-17 06:50:08 -0300

    laço for

---
+++ 04_ling_tecnicas-prog_unidade04/questao_01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int x, i, soma = 0;
+    int y = 10;
+    for(i = 0; i < y; i++){
+        printf("Digite o item [ %d ]: ", i);
+        scanf("%d", &x);
+        soma += x;
+        y -= 1;
+    }
+    
+    printf("TOTAL: %d", soma);
+    return 0;
+
+}
