e4072a0beed405a7796955457735c34b1016f164
Bernardo <mrochabernardo@gmail.com>
2022-10-17 06:57:48 -0300

    começo de códigos em C

--- 01_ling-tecnicas-prog_unidade01/variaveis.c
+++ 01_ling-tecnicas-prog_unidade01/variaveis.c
@@ -4,11 +4,13 @@
 void main()
 {
     printf("Hello word\n");
-
+    printf("Deu certo? \n");
     char letra = 'm';
     int idade = 78;
     const float PI = 3.141592;
     bool tomei_cafe = false;
+
+    printf("Meu nome é %s, tenho %d anos e o meu aniversario é %s/%s","Francisco",52,"12","06\n") ;
     
 
 
