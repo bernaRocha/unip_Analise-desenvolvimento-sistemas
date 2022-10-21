3e286f05734d878e703c701a5701eaa258a63718
Bernardo <mrochabernardo@gmail.com>
2022-10-21 17:52:57 -0300

    exibe média das 5 notas

--- 05_ling_tecnicas-prog_unidade05/preenchendo-vetor.c
+++ 05_ling_tecnicas-prog_unidade05/preenchendo-vetor.c
@@ -2,14 +2,19 @@
 
 int main(){
     float notas[5];
+    float somatorio = 0;
+    float media;
 
     printf("Preenchendo com valores o vetor: \n\n");
 
     for(int i = 0; i <= 4; i++) {
         printf("Nota [%d] = ", i);
-        scanf("%.2f", &notas[i]);
+        scanf("%f", &notas[i]);
         printf("\n------------------- \n");
+        somatorio += notas[i];
     }
+
+    media = somatorio / 5;
 
     printf("Exibindo os valores do vetor: \n\n");
     
@@ -17,6 +22,7 @@ int main(){
         printf("Nota [%d] = %.1f\n", i, notas[i]);
     }
     
+    printf("A média do aluno é: %.2f\n", media);
     
     return 0;
 }
