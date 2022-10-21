6a99b43375e80878022ff842243021f83a3f6d9b
Bernardo <mrochabernardo@gmail.com>
2022-10-21 18:34:20 -0300

    exemplo de struct

---
+++ 05_ling_tecnicas-prog_unidade05/structs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/* Structs são estruituras de dados heterogêneos, também conhecidos como registros*/
+
+struct point 
+{
+    float x;
+    float y;
+};
+
+
+int main() {
+        
+    struct point pt, pt2, pt3; 
+
+    pt.x = 6.1; pt.y = 0.3;
+    pt2.x = 1.0; pt2.y = 1.2;
+    pt3.x = 3.1; pt3.y = 5.1;
+
+}
