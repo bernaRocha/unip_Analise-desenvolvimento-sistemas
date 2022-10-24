130008ae599c906b77aa14a2d78ed8b230316481
Bernardo <mrochabernardo@gmail.com>
2022-10-24 16:39:55 -0300

    recursividade

---
+++ 06_ling_tecnicas-prog_unidade06/exemplo_03-funcao-recursiva.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int multiplica(int x, int y) {
+    if(y == 1) {
+        return (x);
+    }else {
+        return(x + multiplica(x, y - 1));
+    }
+}
+
+void main() {
+    printf("\n %d X %d = %d", 2, 5, multiplica(2, 5));
+    printf("\n\n");
+    return 0;
+}
